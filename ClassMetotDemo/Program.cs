dc3f80192bb4085997eeab90b52bb5cfad37efc7
GONCA ORAL <goncak60@gmail.com>
2021-02-14 14:24:02 +0300

    16 Ocak 2021

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musterimanager = new MusteriManager();
+
+            Console.WriteLine("Müşteri Ekleme");
+            musterimanager.Add(3, "Ayşe", true);
+
+            Console.WriteLine("................................");
+
+            Console.WriteLine("Müşteri Listesi");
+            musterimanager.list();
+
+            Console.WriteLine("Müşteri Silme");
+            musterimanager.Delete(2,"Özge");
+        }
+    }
+}
